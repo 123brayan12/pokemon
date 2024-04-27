32809040584dc255634c47a7cb234052fc75d1e5
Brayan Tamiche <brayan.tamiche.g.dbb@gmail.com>
2024-04-27 11:45:21 -0400

    fidt commit

---
+++ src/api/getPokemonOptions.ts
@@ -0,0 +1,17 @@
+import {pokemonApi} from "./pokemonApi";
+import type { Pokemon } from "env";
+
+const getPokemons = (): any=>{
+    const pokemonArr = Array.from(Array(650))
+    return pokemonArr.map((_, index)=> index+1)
+
+}
+
+console.log(getPokemons())
+
+const getPokemonOptions =()=>{
+    const pokemons = getPokemons()
+    console.log(pokemons)
+}
+
+export default getPokemonOptions
